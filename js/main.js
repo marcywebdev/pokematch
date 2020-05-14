8a96e74f859f2dbaad8bf0ed1673ec90934789d1
marcywebdev <marcelinojasso@hotmail.com>
2020-05-13 21:21:51 -0700

    add png images, pokeball cursor, game start/game over/victory overlays

---
+++ js/main.js
@@ -0,0 +1,22 @@
+function ready() {
+  let overlays = Array.from(document.getElementsByClassName('overlay-text'));
+  let cards = Array.from(document.getElementsByClassName('card'));
+
+  overlays.forEach(overlay => {
+    overlay.addEventListener('click', () => {
+      overlay.classList.remove('visible');
+      // game.startGame();
+    });
+  });
+  cards.forEach(card => {
+    card.addEventListener('click', () => {
+      // game.flipCard(card);
+    });
+  });
+}
+
+if (document.readyState === 'loading') {
+  document.addEventListener('DOMContentLoaded', ready());
+} else {
+  ready();
+}
